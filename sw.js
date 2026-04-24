a7b6600ed57e9cdd9572f436323b4e92a678d46c
Daeseong Kim <lemonaatree@gmail.com>
2026-04-24 17:20:21 +0900

    2026-04-24 17:20:21

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'photography-v1';
+const PRECACHE = ['/', '/css/style.css', '/js/photos.js', '/js/colorpicker.js', '/js/main.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
